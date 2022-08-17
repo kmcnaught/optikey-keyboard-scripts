3075682632df13a43fe6c2d4691710a0db711f53
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2022-08-17 17:12:08 +0100

    WIP

--- make_xml.py
+++ make_xml.py
@@ -16,7 +16,7 @@ def prettify(elem):
     reparsed = minidom.parseString(rough_string)    
     return reparsed.toprettyxml(indent="\t")
 
-def add_key(content_node, row, col, width, height, text):
+def add_textkey(content_node, row, col, width, height, text):
 	key = ET.SubElement(content_node,"DynamicKey")
 
 	# Attributes
@@ -30,6 +30,34 @@ def add_key(content_node, row, col, width, height, text):
 	label_elem.text = text
 	text_elem = ET.SubElement(key, "Text")
 	text_elem.text = text
+	action_elem = ET.SubElement(key, "Action")
+	action_elem.text = "BackFromKeyboard"
+
+
+def add_linkkey(content_node, row, col, width, height, text, link):
+	key = ET.SubElement(content_node,"DynamicKey")
+
+	# Attributes
+	key.set('Row', str(row))
+	key.set('Col', str(col))
+	key.set('Width', str(width))
+	key.set('Height', str(height))
+
+	# Elements
+	label_elem = ET.SubElement(key, "Label")
+	label_elem.text = split_label(text)
+	link_elem = ET.SubElement(key, "ChangeKeyboard")
+	link_elem.text = link
+	link_elem.set('BackReturnsHere', "True")
+
+def split_label(text):
+	L = len(text)
+	if (L > 4):
+		split_length = (int)((L+1)/2)
+		t1 = text[0:split_length]
+		t2 = text[split_length:]
+		text = t1 + "\n" + t2
+	return text
 
 def save_file(xml_root, filename):
 	text = prettify(xml_root)
@@ -63,6 +91,8 @@ def make_text_keyboard(all_chars):
 	tree, content = setup_keyboard()
 
 
+	# these are now in the skeleton
+
 	# suggestions_element = ET.fromstring("<SuggestionRow Width=\"" + str(total_cols) +"\"/>")
 	# scratchpad_element = ET.fromstring("<Scratchpad Width=\"" + str(total_cols) +"\"/>")		
 
@@ -73,30 +103,34 @@ def make_text_keyboard(all_chars):
 	curr_row = 2 # use enumerate for less verbose indexing
 	curr_col = 0
 	for char in all_chars:		
-		add_key(content, curr_row, curr_col, 1, 1, char)
+		add_textkey(content, curr_row, curr_col, 1, 1, char)
 		curr_col += 1
 		if curr_col >= total_cols:
 			curr_col = 0
 			curr_row += 1
 
-	save_file(tree.getroot(), safe_ascii("z__sub-" + all_chars+ ".xml"))
+	fname = safe_ascii("z__sub-" + all_chars+ ".xml")
+	save_file(tree.getroot(), fname)
+	return fname
+
+
 
 total_rows = 4
 total_cols = 4 
 
 # Content node contains all the keys
-tree, content = setup_keyboard()
+tree, content = setup_keyboard(False)
 
 keys = ["abcdefgh", "ijklmnop", "qrstuvwx", "yz?!,;.",] #todo: split with newline for label
 # TODO special char ␣ will need special consideration
 
-# Add keys one by one
+# Add keys to top level one by one
 
-curr_row = 0
+curr_row = 2
 curr_col = 0
 for key in keys:
-	make_text_keyboard(key)
-	add_key(content, curr_row, curr_col, 1, 1, key)
+	link = make_text_keyboard(key)
+	add_linkkey(content, curr_row, curr_col, 1, 1, key, link)
 	curr_col += 1
 	if curr_col >= total_cols:
 		curr_col = 0
@@ -105,4 +139,4 @@ for key in keys:
 #TODO: think about how we keep track of links vs text, text vs actions
 
 
-save_file(tree.getroot(), "wibble.xml")
+save_file(tree.getroot(), "top.xml")
